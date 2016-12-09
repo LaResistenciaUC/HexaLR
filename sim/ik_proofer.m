d8eb1800c3e5b608aa4b4d5cbb51e29b8e2a04a3
Lucas Alvarez <llalvarez@uc.cl>
2016-12-09 12:05:17 -0300

    Changed values on proofer. DO NOT RUN with 400 value range!

--- sim/ik_proofer.m
+++ sim/ik_proofer.m
@@ -1,12 +1,17 @@
-L1=30;L2=92;L3=169;
-for x_ = -50:50
-    for y_ = -50:50
-        for z_ = -50:50
+L1=30;L2=92;L3=169;err=0;
+tStart = tic;
+parfor x_ = -200:200
+    for y_ = -200:200
+        for z_ = -200:200
             try
                 ik(x_,y_,z_,L1,L2,L3);
             catch
-                disp(['r is equal to ',num2str(x_),',',num2str(y_),',',num2str(z_),' .'])
+                err=err+1;
+%                 disp(['r is equal to ',num2str(x_),',',num2str(y_),',',num2str(z_),' .'])
             end
         end
     end
+    tElapsed = toc(tStart);
 end
+err
+disp(['porcentage of error is',num2str((err_/(400^3))*100),' %'])
